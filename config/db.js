554b6c9867b27249160bb2699b69b4daa3f2f45e
mike mwanzi <mikeabere@gmail.com>
2026-05-13 11:56:43 +0300

    import dotenv at top and install morgan

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const connectDB = async ()=> {
     console.log("MongoDB connected");
     
   } catch (error) {
-    console.error();
+    console.error(error);
     process.exit(1);
   }
 
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+import dotenv from "dotenv"; //must import at the top
+dotenv.config();
 import express from "express";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
+
 
 const app = express();
 connectDB();
 
-
+if(process.env.NODE_ENV = "development"){
+   app.use(morgan('dev'));
+}
 
 
 const PORT = process.env.PORT || 7000 ;
